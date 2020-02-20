1576f150ec7ed744b1de868d206a2c00f9944cb8
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2020-02-20 14:25:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const shell = require('electron').shell;
+function createWindow() {
+   // Cree la fenetre du navigateur.
+   const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+         nodeIntegration: true
+      }
+   });
+
+   // et charger le fichier index.html de l'application.
+   win.loadFile('src/index.html');
+
+   win.webContents.openDevTools();
+   // Ouvre les DevTools.
+   win = null;
+}
+const menu = Menu.buildFromTemplate([
+   {
+      label: 'Menu',
+      submenu: [
+         { label: 'Adjust notification value' },
+         {
+            label: 'CoinMarketCap',
+            click() {
+               shell.openExternal('http://coinmarketcap.com');
+            }
+         },
+         { type: 'separator' },
+         {
+            label: 'Exit',
+            click() {
+               app.quit();
+            }
+         }
+      ]
+   },
+   {
+      label: 'Info'
+   }
+]);
+Menu.setApplicationMenu(menu);
+// Cette méthode sera appelée quant Electron aura fini
+// de s'initialiser et prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+   // Sur macOS, il est commun pour une application et leur barre de menu
+   // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+   if (process.platform !== 'darwin') {
+      app.quit();
+   }
+});
+
+app.on('activate', () => {
+   // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+   // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+   if (win === null) {
+      createWindow();
+   }
+});
